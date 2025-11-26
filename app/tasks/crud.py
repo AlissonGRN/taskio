eba88fff310a600a2fd45f19d1a82a056b3f3f5a
AlissonGRN <alissonnascimento.dev@gmail.com>
2025-11-26 20:12:04 -0300

    feat: implement pagination for tasks endpoint

--- app/tasks/crud.py
+++ app/tasks/crud.py
@@ -1,25 +1,45 @@
 import datetime
 import uuid
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, func
 from . import models, schemas
 
 async def get_task_by_id(db: AsyncSession, task_id: str) -> models.Task | None:
     result = await db.execute(select(models.Task).where(models.Task.id == task_id))
     return result.scalar_one_or_none()
 
-async def get_tasks(db: AsyncSession, user_id:uuid.UUID, completed: bool | None = None) -> list[models.Task]:
+async def get_tasks(
+    db: AsyncSession, 
+    user_id:uuid.UUID,
+    page: int,
+    size: int,
+    completed: bool | None = None
+) -> dict:
     
     query = select(models.Task).where(models.Task.owner_id == user_id)
     
-    if completed is True:
-        query = query.where(models.Task.completed == True)
-    elif completed is False:
-        query = query.where(models.Task.completed == False)
-    
-    query = query.order_by(models.Task.created_at)
+    if completed is not None:
+        query = query.where(models.Task.completed == completed)
+
+    count_query = select(func.count()).select_from(query.subquery())
+    total_result = await db.execute(count_query)
+    total = total_result.scalar_one()
+
+    offset = (page - 1)* size
+    query = query.order_by(models.Task.created_at.desc())
+    query = query.offset(offset).limit(size)
+
     result = await db.execute(query)
-    return result.scalars().all()
+    tasks = result.scalars().all()
+    return {
+        "items": tasks,
+        "total": total,
+        "page": page,
+        "size": size,
+        "pages": (total + size - 1) // size if size > 0 else 0 # arredonda as paginas para cima
+    }
+
+    return
 
 async def create_task(db: AsyncSession,user_id:uuid.UUID, task: schemas.TaskCreate) -> models.Task:
     new_task = models.Task(**task.model_dump(), owner_id=user_id)
